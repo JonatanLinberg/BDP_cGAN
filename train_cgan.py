a00ab584450afda05e11e43c802fed22c0b28d8f
JonatanLinberg <98joli@gmail.com>
2021-02-23 15:09:45 +0100

    changed fid-output

--- train_cgan.py
+++ train_cgan.py
@@ -452,8 +452,9 @@ def train(g_model, d_model, gan_model, dataset, latent_dim, fid_model, n_epochs=
 		fid_samples_fake = preprocess_input(fid_samples_fake)
 		fid_samples_real = preprocess_input(fid_samples_real)
 		# calculate fid
+		print("calculating FID with sample size n_real: %d, n_fake%d" % (fid_samples_real.shape[0], fid_samples_fake.shape[0]))
 		fid = calculate_fid(fid_model, fid_samples_fake, fid_samples_real)
-		print(" ->->-> FID for epoch %d: %.03f\n\t(n_real: %d, n_fake%d)" % (i + 1, fid, fid_samples_real.shape[0], fid_samples_fake.shape[0]))
+		print(" ->->-> FID for epoch %d: %.03f" % (i + 1, fid)
 
 		img_ex_count = 10
 		[img_lat_pnt, img_lbl] = generate_latent_points(latent_dim, img_ex_count*rtp_n_classes)
