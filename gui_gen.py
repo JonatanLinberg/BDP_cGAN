6ccb9e434809d9329bea77d58a30d6c97af7cfce
JonatanLinberg <98joli@gmail.com>
2021-02-25 17:55:12 +0100

    Mostly worthless but some code might be salvageable

--- gui_gen.py
+++ gui_gen.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as fig_to_Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as fig_to_tk
+from matplotlib import pyplot
 from numpy.random import randn
+from numpy import zeros
 from functools import partial
 from sys import argv
 from keras.models import load_model
@@ -13,18 +15,15 @@ def generate_latent_point(latent_dim):
 	z_input = x_input.reshape(1, latent_dim)
 	return z_input
 
-def update_fig():
-	print('update_fig')
-
-def update_char(i, new_val):
-	lat_pt[0, i] = new_val
-	if (live):
-		update_fig()
-
-def change_class(new_class):
-	char_class = new_class
-	if (live):
-		update_fig()
+def generate_figure(model, latent_point, class_label, frame):
+	label = zeros((1,), dtype=int)
+	label[0] = class_label
+	out = model.predict([latent_point, label])
+	out = (out + 1) / 2.0
+	fig, ax = pyplot.subplots()
+	pyplot.axis('off')
+	ax = pyplot.imshow(out[0, :, :, 0], cmap='gray_r')
+	return fig 
 
 if (len(argv) > 1):
 	f_name = argv[1]
@@ -32,14 +31,42 @@ else:
 	f_name = input("Enter generator model (.h5) file: ")
 model = load_model(f_name)
 
+root = tk.Tk()
 n_classes = 47
 lat_dim = 100
 n_slider_frames = 4
 lat_pt = generate_latent_point(lat_dim)
 char_class = 10
 live = False
+col_w = 300
+col_h = 700
+red = "#b55"
+green = "#4a6"
 
-root = tk.Tk()
+def update_fig():
+	pass
+
+def update_latent_dim(i, new_val):
+	lat_pt[0, i] = float(new_val.replace(',','.'))
+	if (live):
+		update_fig()
+
+def change_class(new_class):
+	global char_class
+	char_class = new_class
+	if (live):
+		update_fig()
+
+def toggle_live():
+	global live
+	live = not live
+	if (live):
+		live_btn['bg'] = live_btn['activebackground'] = green
+		update_fig()
+	else:
+		live_btn['bg'] = live_btn['activebackground'] = red
+
+
 
 slider_frames = []
 for i in range(n_slider_frames):
@@ -48,14 +75,19 @@ for i in range(n_slider_frames):
 
 
 for i, dim in enumerate(lat_pt[0]):
-	slider = tk.Scale(slider_frames[i // (lat_dim//n_slider_frames)], from_=-3, to=3, resolution=0.001, length=300, repeatdelay=300, orient=tk.HORIZONTAL, command=partial(update_char, i))
+	slider = tk.Scale(slider_frames[i // (lat_dim//n_slider_frames)], from_=-3, to=3, resolution=0.001, length=col_w, repeatdelay=300, orient=tk.HORIZONTAL, command=partial(update_latent_dim, i))
 	slider.set(dim)
 	slider.pack()
 
-char_frame = tk.Frame(root)
+char_frame = tk.Frame(root, width=col_w, height=col_h)
 char_frame.pack(side='left')
-class_slider = tk.Scale(char_frame, from_=1, to=n_classes, orient=tk.HORIZONTAL, command=change_class)
+char_frame.pack_propagate(0)
+class_slider = tk.Scale(char_frame, from_=1, to=n_classes, label="Class ID",orient=tk.HORIZONTAL, command=change_class)
 class_slider.set(char_class)
 class_slider.pack()
+live_btn = tk.Button(char_frame, text='Toggle Live Update', command=toggle_live, bg=red, activebackground=red)
+live_btn.pack()
+
+update_fig()
 
 root.mainloop()
