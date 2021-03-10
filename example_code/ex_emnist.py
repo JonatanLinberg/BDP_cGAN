8632466e1a71e62c8b37878226c55efa95fc56ab
Alexander Gustafsson <62876260+b18alegu@users.noreply.github.com>
2021-03-10 08:08:58 +0100

    HT run 12

--- example_code/ex_emnist.py
+++ example_code/ex_emnist.py
@@ -3,32 +3,34 @@ from tensorflow.keras.datasets.mnist import load_data
 from mnist import MNIST
 from matplotlib import pyplot
 import numpy
-import random
 
-mndata = MNIST('./emnist_data')
+mndata = MNIST('../emnist_data')
 mndata.gz = True
 mndata.select_emnist('byclass')
 
 # load the images into memory
-#(trainX, trainY) = mndata.load_training()
-#trainX, trainY = numpy.asarray(trainX), numpy.asarray(trainY)
 (testX, testY) = mndata.load_testing()
 testX, testY = numpy.asarray(testX), numpy.asarray(testY)
 testX = testX / 255.0
 testX = numpy.reshape(testX, [-1, 28, 28])
 
-#trainX, testX = trainX / 250.0, testX / 250.0
-#(tX, tY), (teX, teY) = load_data()
+
+# Get index of each class
+example_indices = [None] * (max(testY) + 1)
+for i in range(3500):
+	example_indices[testY[i]] = i
+
 
 # plot images from the training dataset
-for i in range(25):
+rows = 5
+cols = 13
+num_classes = len(example_indices)
+for i in range(rows * cols):
 	# define subplot
-	pyplot.subplot(5, 5, 1 + i)
+	pyplot.subplot(rows, cols, 1 + i)
 	# turn off axis
 	pyplot.axis('off')
 	# plot raw pixel data
-	#img = numpy.array(numpy.zeros([28,28]))
-	#for j,px in enumerate(testX[random.randint(0, len(testX))]):
-	#	img[j//28][j%28] = px
-	pyplot.imshow(testX[random.randint(0, len(testX))], cmap='gray_r')
+	pyplot.imshow(testX[example_indices[i % num_classes]], cmap='gray')
+pyplot.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
 pyplot.show()
