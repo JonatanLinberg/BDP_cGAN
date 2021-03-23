df8a0f5375571e518061f1d3a143079415886837
JonatanLinberg <98joli@gmail.com>
2021-03-23 14:19:10 +0100

    G-changes

--- train_cgan.py
+++ train_cgan.py
@@ -364,9 +364,13 @@ def define_generator(latent_dim, n_classes=rtp_n_classes):
 	# upsample to 14x14
 	gen = Conv2DTranspose(rtp_conf_list[rtp_list_index]['g_deconv_filters'], (4,4), strides=(2,2), padding='same')(merge)
 	gen = LeakyReLU(alpha=rtp_conf_list[rtp_list_index]['g_LeReLU_alpha'])(gen)
+	gen = Conv2DTranspose(rtp_conf_list[rtp_list_index]['g_deconv_filters'], (4,4), strides=(1,1), padding='same')(gen)
+	gen = LeakyReLU(alpha=rtp_conf_list[rtp_list_index]['g_LeReLU_alpha'])(gen)
 	#print(gen.shape)
 	# upsample to 28x28
 	gen = Conv2DTranspose(rtp_conf_list[rtp_list_index]['g_deconv_filters'], (4,4), strides=(2,2), padding='same')(gen)
+	gen = LeakyReLU(alpha=rtp_conf_list[rtp_list_index]['g_LeReLU_alpha'])(gen)
+	gen = Conv2DTranspose(rtp_conf_list[rtp_list_index]['g_deconv_filters'], (4,4), strides=(1,1), padding='same')(gen)
 	gen = LeakyReLU(alpha=rtp_conf_list[rtp_list_index]['g_LeReLU_alpha'])(gen)
 	#print(gen.shape)
 	# output
