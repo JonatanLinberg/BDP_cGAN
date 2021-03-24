99e8051015295ce0039eb2fe7b6a950f96068b31
JonatanLinberg <98joli@gmail.com>
2021-03-24 14:15:50 +0100

    update pixel_diff

--- misc_src/pixel_diff.py
+++ misc_src/pixel_diff.py
@@ -5,6 +5,7 @@ from numpy import append
 from numpy import reshape
 from matplotlib import pyplot as plt
 from sys import argv
+import math
 
 def plot_imgs(images):
 	c_str = 'gray_r'
@@ -12,7 +13,7 @@ def plot_imgs(images):
 	for i, _ in enumerate(images):
 		ax[i].tick_params(which='both', bottom=False, top=False, left=False, right=False, labelbottom=False, labeltop=False, labelleft=False, labelright=False)
 		if (i == images.shape[0]-1):
-			c_str = "Reds"
+			c_str = "seismic"
 		ax[i].imshow(images[i, :, :, 0], cmap=c_str)
 	#plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
 	plt.show()
@@ -21,8 +22,8 @@ def create_diff_img(img_a, img_b):
 	img_c = zeros((img_a.shape), dtype='float32')
 	for i, _ in enumerate(img_a):
 		for j, _ in enumerate(img_a[i]):
-			img_c[i, j] = max((img_a[i, j] - img_b[i, j]), 0)
-	img_c[img_c[:, :, 0] <= 0.05] = None
+			img_c[i, j] = (img_a[i, j] - img_b[i, j])
+	img_c[abs(img_c[:, :, 0]) <= 0.01] = None
 	return img_c
 
 
