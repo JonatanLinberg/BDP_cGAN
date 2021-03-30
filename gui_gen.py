4c5885fb0b60630a0f7ec15cd5d84405ca3c7059
JonatanLinberg <98joli@gmail.com>
2021-03-30 15:47:08 +0200

    added traversal animation

--- gui_gen.py
+++ gui_gen.py
@@ -11,6 +11,7 @@ from numpy import array
 from functools import partial
 from sys import argv
 import os
+from datetime import datetime as dt
 from math import ceil
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
@@ -48,6 +49,11 @@ except:
 	quit()
 
 fig_update_interval = 350
+trav_anim_interval = 500
+trav_vec_min = -500
+trav_vec_max = 500
+red = "#EE2211"
+green = "#11BB2A"
 n_latent_dim = model.layers[1].input_shape[0][1] # I think this *always* works
 n_slider_frames = 5
 col_w = 300
@@ -70,6 +76,10 @@ class GuiGen(tk.Frame):
 		self.n_classes.set(47)
 		self.saved_vector = zeros((1, n_latent_dim), dtype='float32')
 		self.base_vector = zeros((1, n_latent_dim), dtype='float32')
+		self.trav_anim = False
+		self.travAnimBtn = None
+		self.trav_step_size = tk.IntVar()
+		self.trav_step_size.set(50)
 		self.createGUI()
 
 	def createGUI(self):
@@ -95,23 +105,27 @@ class GuiGen(tk.Frame):
 		self.char_frame.pack(side='left')
 		self.n_classes.trace_add('write', self.create_class_slider)
 		nc_label = tk.Label(self.char_frame, text='n_classes:', width=col_w//2)
-		nc_label.place(relx=0.4, rely=0.45, anchor='c')
+		nc_label.place(relx=0.4, rely=0.55, anchor='c')
 		class_bound_field = tk.Entry(self.char_frame, textvariable=self.n_classes, width=3)
-		class_bound_field.place(relx=0.7, rely=0.45, anchor='c')
+		class_bound_field.place(relx=0.7, rely=0.55, anchor='c')
 		self.create_class_slider()
 		self.randomBound = tk.Scale(self.char_frame, from_=0, to=3*lat_scale, label='Random Bound', length=col_w, orient=tk.HORIZONTAL)
 		self.randomBound.set(3*lat_scale)
-		self.randomBound.place(relx=0.5, rely=0.30, anchor='c')
+		self.randomBound.place(relx=0.5, rely=0.4, anchor='c')
 		randomiseBtn = tk.Button(self.char_frame, text='Randomise', command=self.randomise_latent_point)
-		randomiseBtn.place(relx=0.5, rely=0.25, anchor='c')
+		randomiseBtn.place(relx=0.5, rely=0.35, anchor='c')
 		normaliseBtn = tk.Button(self.char_frame, text='Normalise', command=self.normalise_latent_point)
-		normaliseBtn.place(relx=0.5, rely=0.2, anchor='c')
-		saveBtn = tk.Button(self.char_frame, text='Save Vector', command=self.save_vector)
-		saveBtn.place(relx=0.5, rely=0.10, anchor='c')
-		saveBtn = tk.Button(self.char_frame, text='Save Base Vector', command=self.save_base_vector)
-		saveBtn.place(relx=0.5, rely=0.05, anchor='c')
-		self.vectorSlider = tk.Scale(self.char_frame, from_=-500, to=500, label='Traverse Vector', length=col_w, orient=tk.HORIZONTAL, command=self.set_vector_percent)
-		self.vectorSlider.place(relx=0.5, rely=0.15, anchor='c')
+		normaliseBtn.place(relx=0.5, rely=0.3, anchor='c')
+		self.vectorSlider = tk.Scale(self.char_frame, from_=trav_vec_min, to=trav_vec_max, label='Traverse Vector', length=col_w, orient=tk.HORIZONTAL, command=self.set_vector_percent)
+		self.vectorSlider.place(relx=0.5, rely=0.21, anchor='c')
+		saveVecBtn = tk.Button(self.char_frame, text='Save Vector', command=self.save_vector)
+		saveVecBtn.place(relx=0.5, rely=0.18, anchor='c')
+		saveBaseBtn = tk.Button(self.char_frame, text='Save Base Vector', command=self.save_base_vector)
+		saveBaseBtn.place(relx=0.5, rely=0.14, anchor='c')
+		self.travAnimBtn = tk.Button(self.char_frame, text='Start Vector Animation', fg=red, command=self.animate_traversal)
+		self.travAnimBtn.place(relx=0.5, rely=0.11, anchor='c')
+		step_slider = tk.Scale(self.char_frame, label='Animation Step Size', variable=self.trav_step_size, from_=0, to=trav_vec_max, length=col_w, orient=tk.HORIZONTAL)
+		step_slider.place(relx=0.5, rely=0.05, anchor='c')
 		self.figureFrame = tk.Frame(self.char_frame)
 		self.set_should_update_figure(True)
 		self.parent.after(0, self.updateFigure)
@@ -155,22 +169,51 @@ class GuiGen(tk.Frame):
 		self.char_class = int(new_class)
 		self.set_should_update_figure(True)
 
+	def animate_traversal(self):
+		self.trav_anim = not self.trav_anim
+		if (self.trav_anim):
+			self.travAnimBtn.configure(fg=green, text="Stop Vector Animation")
+			self.step_trav_anim()
+		else:
+			self.travAnimBtn.configure(fg=red, text="Start Vector Animation")
+
+	def step_trav_anim(self):
+		if (self.trav_anim):
+			vec_val = self.vectorSlider.get() + self.trav_step_size.get()
+			if (vec_val <= trav_vec_max):
+				self.vectorSlider.set(vec_val)
+			else:
+				class_val = self.class_slider.get() + 1
+				if (class_val <= self.n_classes.get()):
+					self.class_slider.set(class_val)
+				else:
+					self.class_slider.set(0)
+				self.vectorSlider.set(trav_vec_min)
+			self._updateFigure() # direct call to figure update
+			self.parent.after(trav_anim_interval, self.step_trav_anim)
+
 	def updateFigure(self):
-		if (self.should_update_figure):
+		if (self.should_update_figure and not self.trav_anim):
 			self.set_should_update_figure(False)
-			self.figureFrame.destroy()
-			self.figureFrame = tk.Frame(self.char_frame)
-			self.figureFrame.place(relx=0.5, rely=0.75, anchor='c')
-			tk_fig = FigureCanvasTkAgg(generate_figure(model, self.lat_pt, self.char_class), self.figureFrame)
-			tk_fig.get_tk_widget().pack()
+			self._updateFigure()
 		self.parent.after(fig_update_interval, self.updateFigure)
+
+	def _updateFigure(self):
+		char_fig = generate_figure(model, self.lat_pt, self.char_class)
+		tempFrame = tk.Frame(self.char_frame)
+		tempFrame.place(relx=0.5, rely=0.75, anchor='c')
+		tk_fig = FigureCanvasTkAgg(char_fig, tempFrame)
+		tk_fig.get_tk_widget().pack()
+		
+		self.figureFrame.destroy()
+		self.figureFrame = tempFrame
 
 	def create_class_slider(self, *args):
 		if (type(self.class_slider) is not NoneType):
 			self.class_slider.destroy()
 		try:
 			self.class_slider = tk.Scale(self.char_frame, repeatdelay=1000, repeatinterval=1000, from_=0, to=self.n_classes.get()-1, length=col_w, label="Class ID",orient=tk.HORIZONTAL, command=self.update_char_class)
-			self.class_slider.place(relx=0.5, rely=0.4, anchor='c')
+			self.class_slider.place(relx=0.5, rely=0.5, anchor='c')
 		except Exception:
 			pass
 		
