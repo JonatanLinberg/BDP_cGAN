2e94abdc22fbcac0223f1ff0bdd3d0477a37076e
JonatanLinberg <98joli@gmail.com>
2021-04-27 14:51:12 +0200

    changed to loss ratio

--- plot_results.py
+++ plot_results.py
@@ -20,13 +20,15 @@ if (len(filenames) == 0):
 if (len(filenames) > 1):
 	average_p_col = int(input("1. Average results\n2. Show as subplots\nChoose: ")) == 1
 
+win_size = int(input("Enter window size: "))
+
 for file in filenames:
 	print("Reading", file, "as CSV file...")
 	df = pd.read_csv(file, names=['d_loss_real','d_loss_fake','g_loss', 'd_acc_real', 'd_acc_fake','FID'])
 	#print(df.head())
 	df = df.interpolate()
 	#print(df.head())
-	df = df.rolling(int(input("Enter window size: "))).mean()
+	df = df.rolling(win_size).mean()
 	df_list.append(df)
 
 if (average_p_col):
@@ -47,17 +49,18 @@ if (len(df_list) > 1):
 	for i in range(len(df_list)):
 		ax[i, 0].plot(df_list[i][['d_loss_real','d_loss_fake','g_loss']])
 		ax[i, 0].legend(('d_loss_real','d_loss_fake','g_loss'))
-		ax[i, 0].set_ylim(top=3)
+		ax[i, 0].set_ylim(top=5)
 		ax[i, 0].set_ylabel('loss')
 		ax[i, 1].plot(df_list[i][['d_acc_real', 'd_acc_fake']])
 		ax[i, 1].legend(('d_acc_real', 'd_acc_fake'))
 		ax[i, 1].set_ylim(top=1)
 		ax[i, 1].set_ylabel('accuracy')
 		ax[i, 2].plot(df_list[i]['FID'])
+		ax[i, 2].set_ylim(top=8)
 		ax[i, 2].set_ylabel('FID')
-		ax[i, 3].plot((df_list[i]['g_loss'] - ((df_list[i]['d_loss_fake'] + df_list[i]['d_loss_real']) / 2)))
-		ax[i, 3].set_ylim(top=1.5)
-		ax[i, 3].set_ylabel('Loss difference')
+		ax[i, 3].plot((df_list[i]['g_loss'] / ((df_list[i]['d_loss_fake'] + df_list[i]['d_loss_real']) / 2)))
+		ax[i, 3].set_ylim(top=10)
+		ax[i, 3].set_ylabel('Loss ratio')
 	for i in range(len(ax)):
 		ax[i, 0].set_title(filenames[i], loc='left')
 		for a in ax[i]:
@@ -66,7 +69,7 @@ if (len(df_list) > 1):
 			a.set_xticks(arange(0, x_len+1, x_len//x_steps, dtype=int))
 			a.set_ylim(bottom=0)
 else:
-	choice = int(input('1. all plots\n2. loss only\n3. accuracy only\n4. FID only\n5. Loss difference\nChoose: '))
+	choice = int(input('1. all plots\n2. loss only\n3. accuracy only\n4. FID only\n5. Loss ratio\nChoose: '))
 	if (choice == 1):
 		plot, sub = plt.subplots(3, figsize=(col_w, col_h))
 		sub[0].plot(df[['d_loss_real','d_loss_fake','g_loss']])
@@ -93,7 +96,7 @@ else:
 		elif (choice == 5):
 			(df['g_loss'] - ((df['d_loss_fake'] + df['d_loss_real'])/2)).plot()
 			plt.ylim(top=1.5)
-			plt.ylabel('Loss difference')
+			plt.ylabel('Loss ratio')
 		plt.xlabel('number of batches')
 		plt.ylim(bottom=0)
 
